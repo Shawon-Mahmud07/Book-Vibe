2ab22b6929e9fb025c4dcad001beaadc2fd0b2d9
Shawon-Mahmud07 <shawon.phero@gmail.com>
2026-05-21 23:57:03 +0600

    feat: add ProtectedRoute for Profile component in router

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -85,6 +85,14 @@ const router = createBrowserRouter([
         ),
       },
       {
+        path: "/profile",
+        element: (
+          <ProtectedRoute>
+            <Profile />
+          </ProtectedRoute>
+        ),
+      },
+      {
         path: "*",
         element: <NotFound />,
       },
